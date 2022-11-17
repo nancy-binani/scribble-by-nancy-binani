53b670ed347b2bf76151fd752bf729bce1241681
Nancy Binani <nancybinani1207@gmail.com>
2022-11-17 12:25:38 +0530

    Removed unecessary show before action

--- app/controllers/api/admin/redirections_controller.rb
+++ app/controllers/api/admin/redirections_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::Admin::RedirectionsController < ApplicationController
-  before_action :load_redirection!, only: %i[ show update destroy ]
+  before_action :load_redirection!, only: %i[ update destroy ]
   before_action :current_site
 
   def index
