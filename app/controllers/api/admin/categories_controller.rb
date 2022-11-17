c5ecfc95a584e002e21c97e2bb99d49355bdf584
Nancy Binani <nancybinani1207@gmail.com>
2022-11-17 12:18:35 +0530

    Custom action kept at last

--- app/controllers/api/admin/categories_controller.rb
+++ app/controllers/api/admin/categories_controller.rb
@@ -19,14 +19,14 @@ class Api::Admin::CategoriesController < ApplicationController
     respond_with_success(t("successfully_updated", entity: "Category"))
   end
 
-  def update_with_position
-    @category.insert_at(params[:category][:position].to_i)
-    respond_with_success(t("successfully_updated", entity: "Category"))
-  end
-
   def destroy
     DeleteCategoryService.new(params[:category], @category, current_user).process
     respond_with_success(t("successfully_deleted", entity: "Category"))
+  end
+
+  def update_with_position
+    @category.insert_at(params[:category][:position].to_i)
+    respond_with_success(t("successfully_updated", entity: "Category"))
   end
 
   private
