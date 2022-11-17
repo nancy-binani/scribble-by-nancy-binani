9f21982ccfb8ac801528fc0e3ce0ebc5bf529689
NancyBinani <71276756+nancy-binani@users.noreply.github.com>
2022-11-17 12:26:21 +0530

    Removed unecessary show before action (#106)

--- app/controllers/api/public/categories_controller.rb
+++ app/controllers/api/public/categories_controller.rb
@@ -1,20 +1,9 @@
 # frozen_string_literal: true
 
 class Api::Public::CategoriesController < ApplicationController
-  before_action :load_category!, only: %i[ show ]
   before_action :current_user
 
   def index
     @categories = current_user.categories.order(:position)
   end
-
-  private
-
-    def load_category!
-      @category = current_user.categories.find(params[:id])
-    end
-
-    def category_params
-      params.require(:category).permit(:category, :position)
-    end
 end
