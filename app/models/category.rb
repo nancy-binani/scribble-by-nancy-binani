85b695eb540e9f253b67f74df63c5cb6e76c49c2
Nancy Binani <nancybinani1207@gmail.com>
2022-11-17 12:58:59 +0530

    Removed wrong enum

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,5 @@ class Category < ApplicationRecord
   has_many :articles
   belongs_to :user
   validates :category, uniqueness: true
-  enum status: { checked: "checked", unchecked: "unchecked" }
   acts_as_list
 end
