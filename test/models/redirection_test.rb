bc9f20a19a96338975002f5112b13d9c396328f6
Nancy Binani <nancybinani1207@gmail.com>
2022-11-24 18:20:00 +0530

    Added tests for redirection

--- test/models/redirection_test.rb
+++ test/models/redirection_test.rb
@@ -13,20 +13,26 @@ class RedirectionTest < ActiveSupport::TestCase
     assert_not new_redirection.valid?
   end
 
-  def test_cyclic_loop
-    redirection_1 = Redirection.create(to: "/1", from: "/2", site: @site)
-    redirection_2 = Redirection.create(to: "/2", from: "/3", site: @site)
-    redirection_3 = Redirection.create(to: "/3", from: "/4", site: @site)
-    redirection_4 = Redirection.create(to: "/4", from: "/1", site: @site)
+  def test_cyclic_loop_one
+    redirection_1 = Redirection.create(from: "/1", to: "/2", site: @site)
+    redirection_2 = Redirection.create(from: "/2", to: "/3", site: @site)
+    redirection_3 = Redirection.create(from: "/3", to: "/4", site: @site)
+    redirection_4 = Redirection.create(from: "/4", to: "/1", site: @site)
     assert_equal redirection_4.errors.full_messages.to_sentence,
       t("redirection.check_redirection_loop")
   end
 
+  def test_cyclic_loop_two
+    redirection_1 = Redirection.create(from: "/1", to: "/2", site: @site)
+    redirection_2 = Redirection.create(from: "/1", to: "/3", site: @site)
+    assert_not redirection_2.valid?
+  end
+
   def test_non_cyclic_loop
-    redirection_1 = Redirection.create!(to: "/3", from: "/2", site: @site)
-    redirection_2 = Redirection.create!(to: "/5", from: "/3", site: @site)
-    redirection_3 = Redirection.create!(to: "/2", from: "/4", site: @site)
-    redirection_4 = Redirection.new(to: "/1", from: "/2", site: @site)
+    redirection_1 = Redirection.create!(from: "/3", to: "/2", site: @site)
+    redirection_2 = Redirection.create!(from: "/5", to: "/3", site: @site)
+    redirection_3 = Redirection.create!(from: "/2", to: "/4", site: @site)
+    redirection_4 = Redirection.new(from: "/1", to: "/2", site: @site)
     assert redirection_4.valid?
   end
 
